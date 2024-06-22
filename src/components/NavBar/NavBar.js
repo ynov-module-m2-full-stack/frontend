7b988a2680090434b2558a74e82071d87199e131
Leonie Mandah <andjibyleonieexode.mandah@ynov.com>
2024-06-22 17:09:43 +0200

    Login and Sigin Modal forms

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,42 @@
-// src/components/NavBar.js
-import React from 'react';
-import './NavBar.css'; 
+import React, { useState } from 'react';
+import LoginModal from '../LoginModal/LoginModal.js';
+import InscriptionModal from '../InscriptionModal/InscriptionModal.js';
+import './NavBar.css';
 
 const NavBar = () => {
+  const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
+  const [inscriptionModalIsOpen, setInscriptionModalIsOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setLoginModalIsOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setLoginModalIsOpen(false);
+  };
+
+  const openInscriptionModal = () => {
+    setInscriptionModalIsOpen(true);
+  };
+
+  const closeInscriptionModal = () => {
+    setInscriptionModalIsOpen(false);
+  };
+
   return (
     <header>
-    <nav>
-      <div className="logo">
-        <a href="/">
-          <img src="/assets/LOGO.png" alt="Logo" className="logo-img" /> 
-        </a>
-      </div>
-      {/* <div className="video-container">
-      <video className="video" loop autoPlay muted>
-        <source src="./assets/video.mp4" type="video/mp4" />
-      </video></div> */}
-      <div className="log">
-      <div className="log1">
-      <a href="/connexion" className="nav-link">Connexion</a>
-      </div>
-      <div className="log2"> <a href="/inscription" className="nav-link">Inscription</a></div></div>
+      <nav>
+        <div className="logo">
+          <a href="/">
+            <img src="/assets/LOGO.png" alt="Logo" className="logo-img" /> 
+          </a>
+        </div>
+        <div className="log">
+          <button className="log1" onClick={openLoginModal}>Connexion</button>
+          <LoginModal isOpen={loginModalIsOpen} onRequestClose={closeLoginModal} />
+          <button className="log2" onClick={openInscriptionModal}>Inscription</button>
+          <InscriptionModal isOpen={inscriptionModalIsOpen} onRequestClose={closeInscriptionModal} />
+        </div>
       </nav>
     </header>
   );
